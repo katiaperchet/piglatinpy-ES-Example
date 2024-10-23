42145c79860ba3baf97e8a4f0d8f1e34aa23be42
katiaperchet <katiamperchet@gmail.com>
2024-10-23 16:16:58 +0200

    Red phase of US3

--- test/test_translator.py
+++ test/test_translator.py
@@ -20,3 +20,11 @@ class TestPigLatinTranslator(TestCase):
         # Assert
         self.assertEqual("nil", translation)
 
+    def test_translate_word_start_vowel_ending_y(self):
+        # Arrange
+        translator = PigLatinTranslator("any")
+        # Act
+        translation = translator.translate()
+        # Assert
+        self.assertEqual("anynay", translation)
+
